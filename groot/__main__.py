ce0cc656283760b1fabaa8d2b3c2bc15d3ea1746
Spark <113758022+RobinMusiczz@users.noreply.github.com>
2023-03-20 07:36:46 +0530

    Update __main__.py

--- groot/__main__.py
+++ groot/__main__.py
@@ -1,3 +1,9 @@
+# GROOT NETWORK
+# G-Network Projects
+# Copyright (C) 2023 By @Groot_Network
+# Copy Rights @RJbr0 , @MyNameIsGROOT
+# Don't Any Value In This Repo If You Edit Your Github Will Get Banned 😌
+
 from pyrogram import idle
 from . import groot
 groot.start()
